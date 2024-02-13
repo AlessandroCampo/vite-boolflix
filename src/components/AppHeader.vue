5d3066f9ff716e4ce58d8fef5ccd0ebf85565bea
AlessandroCampo <alessandrocampo97@gmail.com>
2024-02-13 12:35:02 +0100

    feat: header+logo+favicon

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="header-container">
+        <figure>
+            <img src="../assets/img/logo.png" alt="">
+        </figure>
+
+        <nav>
+            <ul>
+                <li> Home </li>
+                <li> Series </li>
+                <li> Movies </li>
+            </ul>
+        </nav>
+
+
+
+        <div class="header-right">
+            <div class="searchbar">
+
+                <button @click="getMovies()">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <input type="text" v-model="store.searchString">
+            </div>
+            <dv class="account">
+                A
+            </dv>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+
+import { store } from "../store"
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        getMovies() {
+            console.log("clicked")
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${store.apiKey}&query=${store.searchString}`).then((res) => {
+                console.log(res.data);
+            });
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-block: 1.5em;
+
+    figure {
+        flex-basis: 25%;
+
+        img {
+            width: 50%;
+        }
+    }
+
+    nav {
+        flex-basis: 50%;
+        display: flex;
+        justify-content: center;
+
+        ul {
+            display: flex;
+            gap: 2em;
+            font-size: 20px;
+        }
+    }
+
+
+
+    .header-right {
+
+        display: flex;
+        align-items: center;
+        gap: 2em;
+        justify-content: flex-end;
+        border: 2px;
+        flex-basis: 25%;
+
+        .account {
+            background-color: white;
+            color: #1a2430;
+            width: 50px;
+            height: 50px;
+            aspect-ratio: 1;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            clip-path: circle();
+            font-size: 20px;
+            font-weight: bold;
+
+        }
+
+        .searchbar {
+            display: flex;
+            align-items: center;
+            gap: 2em;
+
+            button {
+                font-size: 25px;
+                display: flex;
+                align-items: center;
+            }
+
+            input {
+                background-color: #1a2430;
+                border-radius: 10px;
+                border: 1px solid white;
+                padding-left: 5px;
+            }
+        }
+    }
+
+
+
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import envCompatible from 'vite-plugin-env-compatible';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  envPrefix: "API_KEY",
+  plugins: [vue(), envCompatible()],
 })
